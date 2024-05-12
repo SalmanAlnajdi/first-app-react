22b88b7739dfb446610993042abbbace6fce9546
SalmanAlnajdi <salman.it095@gmail.com>
2024-05-12 19:17:41 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Title from './components/Title';
+import styles from './mystyle.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return(
+   <div className={styles.welcome}>
+    <Title/>
+    <img src='https://images.pexels.com/photos/1423600/pexels-photo-1423600.jpeg?cs=srgb&dl=pexels-jplenio-1423600.jpg&fm=jpg'></img>
+   </div>
+ );
 }
 
 export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+const Title = ()=>{
+    return(
+
+        <div>
+            <h1 >Salman Alnajdi</h1>
+            <p>
+                welcome to One of the Kuwaiti Small forest!
+            </p>
+        </div>
+    )
+}
+
+
+
+export default Title;
